6389dceedbce869c028d3182cb64ec51f8c05144
will oracions <Oracions.dev@gmail.com>
2021-08-29 11:30:15 +0100

    correct package enty point path and export unexported classes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 export { BaseModel } from './models/base.model';
 export { BaseHttpService } from './services/base-http.service';
+
+// Actions
 export { BaseActions } from './redux/base-action';
+export { getBaseActions } from './redux/base-action';
+export { BaseActionsType } from './redux/base-action';
+// Effects
 export { BaseEffect } from './redux/base-effect';
+// Reducers
 export { BaseReducer } from './redux/base-reducer';
+export { BaseState } from './redux/base-reducer';
+// Selectors
 export { BaseSelector } from './redux/base-selector';
+
+// Dispatcher
 export { HandleDispatch } from './redux/handle-dispatch';
