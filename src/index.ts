f65d9e7f1b431fccd84f7796831a5b0c2c8c0ff6
will oracions <Oracions.dev@gmail.com>
2021-08-29 11:15:28 +0100

    export classes

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { BaseModel } from './models/base.model';
+export { BaseHttpService } from './services/base-http.service';
+export { BaseActions } from './redux/base-action';
+export { BaseEffect } from './redux/base-effect';
+export { BaseReducer } from './redux/base-reducer';
+export { BaseSelector } from './redux/base-selector';
+export { HandleDispatch } from './redux/handle-dispatch';
